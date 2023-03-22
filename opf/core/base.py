dc4dda39131b08ed8e285424c7f2eb09f21085e3
Seonho Park <spark719@login-phoenix-slurm-2.pace.gatech.edu>
2023-03-22 14:42:05 -0400

    add verbose arg

--- opf/core/base.py
+++ opf/core/base.py
@@ -15,5 +15,5 @@ class AbstractPowerBaseModel(ABC):
     def _build_model(self) -> None: pass
 
     @abstractmethod
-    def instantiate_model(self, network:Dict[str,Any], init_var:Dict[str,Any] = None) -> pyo.ConcreteModel: pass
+    def instantiate_model(self, network:Dict[str,Any], init_var:Dict[str,Any] = None, verbose:bool = False) -> pyo.ConcreteModel: pass
 
