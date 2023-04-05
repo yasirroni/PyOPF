14956cb23c9e092c767f756971e5dc109d139efd
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2023-04-05 12:58:09 +0700

    add install.md to install ipopt

--- examples/acopf_pglib.py
+++ examples/acopf_pglib.py
@@ -5,5 +5,5 @@ model = opf.build_model('acopf')
 network = opf.parse_file(pglib_opf_case5_pjm)
 model.instantiate(network)
 result = model.solve(solver='ipopt',
-                     solver_option={'print_level' : 5, 'linear_solver': 'ma27'},
+                    #  solver_option={'print_level' : 5, 'linear_solver': 'ma27'},
                      tee=True)
